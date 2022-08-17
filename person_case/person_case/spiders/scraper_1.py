cbb37c6fd884c504d1058a0d4b620f1c50f71680
arod <alexrodriguez218@yahoo.com>
2022-08-17 18:49:31 +0000

    Initial commit

---
+++ person_case/person_case/spiders/scraper_1.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class CourtScraper(scrapy.Spider):
+  name = 'cases'
+  start_urls = ['https://courtconnect.courts.delaware.gov/cc/cconnect/ck_public_qry_cpty.cp_personcase_srch_details?backto=P&soundex_ind=&partial_ind=checked&last_name=a&last_name=a&first_name=&middle_name=&begin_date=08-AUG-2022&end_date=09-AUG-2022&case_type=ALL&id_code=&PageNo=1']
+  
+
+  def parse(self,response):
+    rows = response.xpath('/html/body/font[1]/table[2]/tr')
+    for i in range(1,len(rows)):
+      yield{
+        'id': rows[i].xpath('td[1]/text()').extract()[0] ,
+        'name':rows[i].xpath('td[2]/text()').extract()[0],
+        'address':rows[i].xpath('td[3]/text()').extract()[0],
+        'party type':rows[i].xpath('td[4]/text()').extract()[0],
+        'party end date':rows[i].xpath('td[5]/text()').extract()[0],
+        'filing date':rows[i].xpath('td[6]/text()').extract()[0],
+        'case status':rows[i].xpath('td[7]/text()').extract()[0]
+      }
+    next_page = response.xpath('//a[contains(text(), "Next")]')
+    if next_page is not None:
+      yield response.follow( response.xpath('//a[contains(text(), "Next")]').attrib['href'], callback=self.parse)
+      
